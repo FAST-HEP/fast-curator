ce5f2d6ac083b1a81abd731faa6ed7bf49e542cf
Ben Krikler <bek07@ic.ac.uk>
2018-10-17 19:02:39 +0200

    Uncomment step to push to Pypi

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.cern.ch/fast-hep/public/fast-curator',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=True,
 )
