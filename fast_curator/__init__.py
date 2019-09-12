75f5607585e036518052d710b007fa36e9df4c1c
Ben Krikler <bek07@ic.ac.uk>
2019-09-12 20:24:29 +0200

    Tidy up init

--- fast_curator/__init__.py
+++ fast_curator/__init__.py
@@ -1,6 +1,10 @@
+"""Top-level package for fast-curator."""
 from . import read
 from . import write
+from .version import __version__, version_info
 
 
-__all__ = ["read", "write"]
-__version__ = "__version__ = '0.2.2'"
+__all__ = ["read", "write", "__version__", "version_info"]
+
+__author__ = """Benjamin Krikler, and F.A.S.T"""
+__email__ = 'fast-hep@cern.ch'
