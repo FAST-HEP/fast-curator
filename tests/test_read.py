e434a0fdc322e29aec58b25e49c2edb4551d4701
Ben Krikler <bek07@ic.ac.uk>
2019-09-28 18:46:15 +0200

    Test read._from_dict function

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,3 +1,4 @@
+import pytest
 import fast_curator.read as fc_read
 
 
@@ -10,6 +11,22 @@ def test__from_string():
     assert len(config) == 2
     assert config["name"] == "dummy_data_2"
     assert config["key"] == "val"
+
+
+def test__from_dict():
+    dataset = dict(one=1, two="2")
+
+    with pytest.raises(RuntimeError) as e:
+        fc_read._from_dict(dataset, {})
+    assert "'name'" in str(e)
+
+    dataset["name"] = "test__from_dict"
+    config = fc_read._from_dict(dataset, dict(one="one", three=333))
+
+    assert len(config.keys()) == 4
+    assert config["one"] == 1
+    assert config["two"] == "2"
+    assert config["three"] == 333
 
 # def associate_by_ext_suffix(datasets):
 # def from_yaml(path, defaults={}, find_associates=associate_by_ext_suffix):
